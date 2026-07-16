5cbb37e4bb3bf5e81eb691a3df68334370fd3be4
Fazl Rahman Sabawoon <sabawoonrahman942@gmail.com>
2026-07-16 12:46:09 +0000

    Initial Todo API project

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+todos = [
+    {"id": 1, "task": "Linux öğren", "done": False},
+    {"id": 2, "task": "Docker öğren", "done": True}
+]
+
+@app.route("/")
+def home():
+    return jsonify({
+        "message": "Todo API çalışıyor"
+    })
+
+@app.route("/todos")
+def get_todos():
+    return jsonify(todos)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
